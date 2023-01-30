635e18b1a5d1cfab647cd4ea4e0830330325b0cc
comvalteam11 <comvalteam11@gmail.com>
2023-01-30 19:25:27 +0530

    t

--- src/app/modules/events/bussiness/events.bussiness.js
+++ src/app/modules/events/bussiness/events.bussiness.js
@@ -27,7 +27,7 @@ const get_event_id = async (req, response) => {
 }
 const add_event = async (req, response) => {
   try {
-    console.log(req.body,"aa")
+    console.log(req.body, "aa")
     if (req.user.role_id == 2) {
       let obj = {
         event_name: req?.body?.event_name ? req?.body?.event_name : '',
@@ -38,12 +38,12 @@ const add_event = async (req, response) => {
         event_organiser: req?.body?.event_organiser ? req?.body?.event_organiser : '',
         event_venue: req?.body?.event_venue ? req?.body?.event_venue : '',
         event_companies: req?.body?.event_companies ? req?.body?.event_companies : '',
-        mobile_no:req?.body?.mobile_no ? req?.body?.mobile_no : '',
+        mobile_no: req?.body?.mobile_no ? req?.body?.mobile_no : '',
         event_website: req?.body?.event_website ? req?.body?.event_website : '',
         event_image: req?.body?.event_image ? req?.body?.event_image : '',
         event_email: req?.body?.event_email ? req?.body?.event_email : '',
-        event_status:req?.body?.event_status ? req?.body?.event_status : 0,
-        site_id:req?.body?.site_id ? req?.body?.site_id : '',
+        event_status: req?.body?.event_status ? req?.body?.event_status : 0,
+        site_id: req?.body?.site_id ? req?.body?.site_id : '',
       }
       var sql = "insert into event(event_name,event_select_sector,event_country,event_date,event_alt_tag,event_organiser,event_venue,event_companies,mobile_no,event_website,event_image,event_email,event_status,site_id) values ?"
       var data1 = [Object.values(obj)]
@@ -73,12 +73,12 @@ const edit_event = async (req, response) => {
         event_organiser: req?.body?.event_organiser ? req?.body?.event_organiser : '',
         event_venue: req?.body?.event_venue ? req?.body?.event_venue : '',
         event_companies: req?.body?.event_companies ? req?.body?.event_companies : '',
-        mobile_no:req?.body?.mobile_no ? req?.body?.mobile_no : '',
+        mobile_no: req?.body?.mobile_no ? req?.body?.mobile_no : '',
         event_website: req?.body?.event_website ? req?.body?.event_website : '',
         event_image: req?.body?.event_image ? req?.body?.event_image : '',
         event_email: req?.body?.event_email ? req?.body?.event_email : '',
-        event_status:req?.body?.event_status ? req?.body?.event_status : 0,
-        site_id:req?.body?.site_id ? req?.body?.site_id : '',
+        event_status: req?.body?.event_status ? req?.body?.event_status : 0,
+        site_id: req?.body?.site_id ? req?.body?.site_id : '',
       }
       var sql = "update event set event_name=?,event_select_sector=?,event_country=?,event_date=?,event_alt_tag=?,event_organiser=?,event_venue=?,event_companies=?,mobile_no=?,event_website=?,event_image=?,event_email=?,event_status=?,site_id=? where event_id=?"
       var data1 = Object.values(obj)
@@ -100,24 +100,28 @@ const edit_event = async (req, response) => {
 const delete_event = async (req, response) => {
   try {
     if (req.user.role_id == 2) {
-      var sql1="select soft_delete from event where event_id=" + req.params.id 
+      var sql1 = "select soft_delete from event where event_id=" + req.params.id
       con.query(sql1, (err, res) => {
         if (err)
           response.json(err);
-        console.log(res)
-      })
 
-      var sql = "delete from event where event_id=" + req.params.id
-      con.query(sql, (err, res) => {
-        if (err)
-          response.json(err);
-        response.json(res)
-      })
-    }
+        if ((res[0]?.soft_delete == '1' || res[0]?.soft_delete == 1)) {
+          response.json("already deleted")
+        }
+        else
+        {
+          var sql = "delete from event where event_id=" + req.params.id
+          con.query(sql, (err, res) => {
+            if (err)
+              response.json(err);
+            response.json(res)
+          })
+        }
+        })
+      } 
     else {
       response.json("unauthorised user").status(401);
     }
-
   }
   catch (err) {
     return err
