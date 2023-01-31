b68e1c7af7b10d2bf70c0ad54164078379637467
comvalteam11 <comvalteam11@gmail.com>
2023-01-31 15:22:05 +0530

    a

--- src/app/modules/admin/bussiness/admin.bussiness.js
+++ src/app/modules/admin/bussiness/admin.bussiness.js
@@ -1102,7 +1102,8 @@ const edit_project_sub_sector2 = async (req, response) => {
 }
 const edit_project_sub_sector3 = async (req, response) => {
   try {
-    var sql = "select * from edit_project_sub_sector"
+    console.log(req.params.id,"a")
+    var sql = `select * from edit_project_sub_sector where soft_delete='0' && site_id='${req.params.id}'`
     con.query(sql, (err, res) => {
       if (err)
         return response.json(err);
